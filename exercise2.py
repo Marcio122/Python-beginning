4a623ba9c40a463d87d6a47f2702483b19e34678
Marcio Geoveti <68748367+Marcio122@users.noreply.github.com>
2021-03-02 12:39:48 +0100

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+# Simple message
+message = "Luanda is a perfect city"
+print(message)
+
+# Simple messages
+
+messages = "I'm glorious"
+print(messages)
+
+messages = "I'm more than a winner"
+print(messages)
