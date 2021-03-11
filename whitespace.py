1fd9cbcbdf27236c171b446b68336c62247ffdca
Marcio Geoveti <68748367+Marcio122@users.noreply.github.com>
2021-03-11 13:21:18 +0100

    Add files via upload

---
+++ whitespace.py
@@ -0,0 +1,8 @@
+# using tabs \t
+print("\tPython")
+
+# to add a newline in Python \n
+print("Favorite Languages:\nPython\nJavaScript")
+
+#  The string "\n\t" tells Python to move to a new line, and start the next line with a tab.
+print("Best languages for me:\n\tPython\n\tJavaScript")
