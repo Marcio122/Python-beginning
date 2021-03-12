826c8553a758cf562c02f5eee6432261e2dbd56f
Marcio Geoveti <68748367+Marcio122@users.noreply.github.com>
2021-03-12 11:55:04 +0100

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,21 @@
+# numbers
+# there are many types of numbers in python
+# integers, float
+
+#Integers
+print(2 + 5)
+print(30 / 2)
+print(2 * 2)
+print(15 - 8)
+print(2 ** 3)
+print((2 + 4) * 5)
+
+# Floats - any number with a decimal point is a float
+print(0.1 + 0.1)
+print(2.5 + 2.5)
+print(10.5 / 5.5)
+
+# if you add a integers with a float you will get a float
+print(5 + 3.0)
+# if you divide two integers you will still get an float
+print(4/2)
